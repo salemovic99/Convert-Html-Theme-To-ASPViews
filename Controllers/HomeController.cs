7ae16994a07ba765e917f8be3b3bef1767fbc0d3
salemovic <jakihop889@gmail.com>
2023-05-12 16:51:57 +0300

    Add Exercise for Lecture 5

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using ConvvertHtmlThemeToAspViews.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConvvertHtmlThemeToAspViews.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Singl()
+        {
+            return View();
+        }
+
+    }
+}
